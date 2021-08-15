f45298f11a87d5cd6f0489e4c3147f4c7a04e7b8
Khoa Huynh Tran Dang <khoa.htd@amanotes.com>
2021-08-15 17:10:57 +0700

    remove report web vitals

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -7,7 +7,6 @@ import './assets/styles/index.scss';
 
 import { App } from './app';
 import createStore from './store';
-import reportWebVitals from './reportWebVitals';
 
 const initialState = window.__INITIAL_STATE__;
 delete window.__INITIAL_STATE__;
@@ -33,8 +32,3 @@ if (module.hot) {
     if (status === 'check') window.location.reload();
   });
 }
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
