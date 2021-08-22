83df70c43798b8a95c476ebb0f9c6fe59e84b622
Khoa Huynh Tran Dang <khoa.htd@amanotes.com>
2021-08-22 21:54:35 +0700

    replace axios to node-fetch and remove react prop types in production

--- src/client/api/index.js
+++ src/client/api/index.js
@@ -1,20 +1,37 @@
-import axios from 'axios';
+import nFetch from 'node-fetch';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 import appConfig from 'configs/app';
 
-const instance = axios.create({
-  baseURL: appConfig.baseUrl,
-});
+if (!globalThis.fetch) {
+  globalThis.fetch = nFetch;
+}
+
+const request = (url, options) =>
+  new Promise((resolve, reject) =>
+    fetch(`${appConfig.baseUrl}${url}`, options)
+      .then((res) => {
+        if (!res.ok) {
+          const err = new Error(res.statusText);
+          err.response = res;
+
+          return reject(err);
+        }
+
+        return res.json();
+      })
+      .then((json) => resolve(json))
+      .catch((err) => reject(err)),
+  );
 
 export const getTodos = createAsyncThunk('getTodos', async () => {
-  const res = await instance.get('todos');
+  const res = await request('todos');
 
-  return res.data;
+  return res;
 });
 
 export const getTodo = createAsyncThunk('getTodo', async (id) => {
-  const res = await instance.get(`todos/${id}`);
+  const res = await request(`todos/${id}`);
 
-  return res.data;
+  return res;
 });
