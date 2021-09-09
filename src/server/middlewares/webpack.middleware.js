d73f686b339a2c9c48692f37bdeeb456c48a6530
Khoa Huynh Tran Dang <khoa.htd@amanotes.com>
2021-09-09 09:15:19 +0700

    add new line when log in webpack middleware

--- src/server/middlewares/webpack.middleware.js
+++ src/server/middlewares/webpack.middleware.js
@@ -32,7 +32,7 @@ const webpackMiddleware = () => {
     console.log(`  ${chalk.bold('Local:')}\t\thttp://localhost:${chalk.bold(serverConfig.PORT)}`);
     console.log(`  ${chalk.bold('On Your Network:')}\thttp://${host}:${chalk.bold(serverConfig.PORT)}\n`);
     console.log('Note that the development build is not optimized.');
-    console.log(`To create a production build, use ${chalk.blue('npm run build')}.`);
+    console.log(`To create a production build, use ${chalk.blue('npm run build')}.\n`);
   });
 
   return [whm(compiler, { log: false, path: '/__webpack_hmr', heartbeat: 200 }), instance];
