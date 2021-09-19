a05cd96c899cdb163c37c91d12970a0e496bb237
Khoa Huynh Tran Dang <khoa.htd@amanotes.com>
2021-09-19 14:41:48 +0700

    update timeout for jest

--- jest/setup-test.js
+++ jest/setup-test.js
@@ -1,2 +1,4 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import '@testing-library/jest-dom';
+
+jest.setTimeout(60000);
--- src/client/api/request.js
+++ src/client/api/request.js
@@ -6,10 +6,10 @@ const request = (url, options) =>
   new Promise((resolve, reject) =>
     fetch(`${appConfig.baseUrl}${url}`, options)
       .then(async (res) => {
-        const raw = await res.text();
-
         if (!res.ok) {
-          const err = new Error(raw);
+          const message = [res.status, res.statusText && `: ${res.statusText}`].filter(Boolean).join('');
+
+          const err = new Error(message);
           err.response = res;
 
           return reject(err);
