510aa9d81b65dc0c43d77aad97b3a8605134a1cb
Khoa Huynh Tran Dang <khoa.htd@amanotes.com>
2021-09-28 11:52:20 +0700

    replace isomorphic-fetch to latest node-fetch and update deps

--- src/client/api/request.js
+++ src/client/api/request.js
@@ -1,10 +1,26 @@
-import 'isomorphic-fetch';
-
 import appConfig from 'configs/app';
 
-const request = (url, options) =>
+/**
+ * Loading node-fetch@3 in CJS
+ * References: https://github.com/node-fetch/node-fetch/issues/1279#issuecomment-915063354
+ */
+// eslint-disable-next-line no-new-func
+const _importDynamic = new Function('modulePath', 'return import(modulePath)');
+
+const nFetch = (...args) => _importDynamic('node-fetch').then(({ default: fetch }) => fetch(...args));
+
+if (!globalThis.fetch) {
+  globalThis.fetch = nFetch;
+}
+
+/**
+ * @param {string} url
+ * @param {RequestInit} init
+ * @returns {Promise<Response>}
+ */
+const request = (url, init) =>
   new Promise((resolve, reject) =>
-    fetch(`${appConfig.baseUrl}${url}`, options)
+    fetch(`${appConfig.baseUrl}${url}`, init)
       .then(async (res) => {
         if (!res.ok) {
           const message = [res.status, res.statusText && `: ${res.statusText}`].filter(Boolean).join('');
