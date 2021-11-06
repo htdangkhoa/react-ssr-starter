f915c7980e5e5fcafab38f9c5143c81e3ca2af58
Khoa Huynh Tran Dang <khoa.htd@amanotes.com>
2021-11-07 01:38:56 +0700

    migrate unit testing with react-router

--- src/client/pages/UserInfo/__tests__/index.test.jsx
+++ src/client/pages/UserInfo/__tests__/index.test.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 import { render, screen, waitFor } from 'test-utils/render';
 import UserInfo from '../index';
 
 test('render <UserInfo />', async () => {
-  render(<UserInfo path='/user-info/:id' />, { useRouter: true, route: '/user-info/1' });
+  render(
+    <Routes>
+      <Route path='/user-info/:id' element={<UserInfo />} />
+    </Routes>,
+    { useRouter: true, path: '/user-info/1' },
+  );
 
   expect(screen.queryByText(/Loading\.\.\./i)).toBeInTheDocument();
 
-  await waitFor(() => screen.findByRole('list'), { timeout: 60000 });
+  await waitFor(() => screen.findByRole('list'), { timeout: 1000 });
 
   const ul = await screen.findByRole('list');
 
--- src/client/pages/UserList/__tests__/index.test.jsx
+++ src/client/pages/UserList/__tests__/index.test.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 import { render, screen, waitFor } from 'test-utils/render';
 import UserList from '../index';
 
 test('render <UserList />', async () => {
-  render(<UserList />);
+  render(
+    <Routes>
+      <Route path='/home' element={<UserList />} />
+    </Routes>,
+    { useRouter: true, path: '/home' },
+  );
 
   expect(screen.queryByText(/Loading\.\.\./i)).toBeInTheDocument();
 
