04083af04cf1a6cc35f85e456396cc522efbb4ca
Khoa Huynh Tran Dang <khoa.htd@amanotes.com>
2021-11-26 17:30:08 +0700

    test env

--- webpack/plugins/dotenv-webpack-plugin.js
+++ webpack/plugins/dotenv-webpack-plugin.js
@@ -73,17 +73,14 @@ class DotenvWebpackPlugin {
       return _env;
     }, {});
 
-    const formattedRaw = Object.keys(raw).reduce((env, key) => {
-      const _env = env;
-      _env[key] = JSON.stringify(raw[key]);
+    // const formattedRaw = Object.keys(raw).reduce((env, key) => {
+    //   const _env = env;
+    //   _env[key] = JSON.stringify(raw[key]);
 
-      return _env;
-    }, {});
+    //   return _env;
+    // }, {});
 
-    new DefinePlugin({
-      'process.env': formattedRaw, // fix: cannot read process object in local
-      ...formattedVars,
-    }).apply(compiler);
+    new DefinePlugin(formattedVars).apply(compiler);
   }
 }
 
